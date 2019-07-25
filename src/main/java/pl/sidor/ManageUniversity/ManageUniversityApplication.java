41d3c2e6d5c3795559e70bd919e30ea8c8b824a7
KursJava <kursjava1@wp.pl>
2019-07-25 19:58:55 +0200

    git init

---
+++ src/main/java/pl/sidor/ManageUniversity/ManageUniversityApplication.java
@@ -0,0 +1,13 @@
+package pl.sidor.ManageUniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageUniversityApplication.class, args);
+	}
+
+}
